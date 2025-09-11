293121532d74cb06e71b3eadf7403917a3f306c5
dikeyhere <andika.dk07@gmail.com>
2025-09-11 09:33:04 +0700

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,29 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World - Laravel 10</title>
+    <title>Project Laravel</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
 
-    <!-- <div class="container text-center py-5">
+    <div class="container text-center py-5">
         <div class="card shadow-lg p-5 rounded-4">
             <h1 class="display-4 fw-bold text-primary mb-3">Hello World!</h1>
             <p class="lead text-secondary">
                 Selamat datang!
             </p>
-            <a href="#" class="btn btn-primary mt-3 px-4">Get Started</a>
-        </div>
-    </div> -->
-
-    <div class="container text-center py-5">
-        <div>
-            <h1 class="display-4 fw-bold mb-3">Hello World!</h1>
-            <p class="lead text-secondary">
-                Selamat datang!
-            </p>
-            <!-- <a href="#" class="btn btn-primary mt-3 px-4">Get Started</a> -->
         </div>
     </div>
 
