eea5cfdbadeb958f6e8f43c8145ecfbd49c41e43
dikeyhere <andika.dk07@gmail.com>
2025-09-24 22:06:32 +0700

    eight commit

--- resources/views/hrd/pegawai.blade.php
+++ resources/views/hrd/pegawai.blade.php
@@ -25,10 +25,77 @@
             overflow: hidden;
         }
     </style>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- Scripts -->
+    @vite(['resources/js/app.js'])
 </head>
 
 <body>
-    <div class="container-fluid mt-4 pt-2 pe-4 ps-4">
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container ps-1 ms-5 me-0 pe-0">
+                <a class="navbar-brand pe-5 me-5" href="{{ url('/home') }}">Wanna Be Studio
+                    <!-- {{ config('app.name', 'Wanna Be Studio') }} -->
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        @guest
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <!-- <a href="{{ route('pegawais.index') }}" class="btn btn-primary btn-sm me-2 text-center align-middle">
+                            Pegawai
+                        </a> -->
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-1">
+            @yield('content')
+        </main>
+    </div>
+
+    <div class="container-fluid mt-0 pt-0 pe-4 ps-4">
         <div class="card p-1 pe-4 ps-4">
             <h2 class="mb-4 mt-2 text-center">Daftar Pegawai</h2>
 
--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PegawaiController;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::get('/pegawai', function () {
 //     return view('hrd/pegawai');
 // });
+// Route::get('/', function () {
+//     return redirect()->route('login');
+// });
 
-Route::resource('pegawais', PegawaiController::class);
-Route::get('pegawais/{id}/restore', [PegawaiController::class, 'restore'])->name('pegawais.restore');
-Route::delete('pegawais/{id}/force-delete', [PegawaiController::class, 'forceDelete'])->name('pegawais.forceDelete');
+Route::get('/', function () {
+    if (!Auth::check()) {
+        return view('auth.login');
+    }
+    return redirect()->route('pegawais.index');
+});
+
+// Route::resource('pegawais', PegawaiController::class);
+// Route::get('pegawais/{id}/restore', [PegawaiController::class, 'restore'])->name('pegawais.restore');
+// Route::delete('pegawais/{id}/force-delete', [PegawaiController::class, 'forceDelete'])->name('pegawais.forceDelete');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('pegawais', PegawaiController::class);
+    Route::get('pegawais/{id}/restore', [PegawaiController::class, 'restore'])
+        ->name('pegawais.restore');
+    Route::delete('pegawais/{id}/force-delete', [PegawaiController::class, 'forceDelete'])
+        ->name('pegawais.forceDelete');
+});
